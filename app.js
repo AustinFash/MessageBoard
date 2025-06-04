3beaa7da4e02e5bfe3e420e2ea99e48d50653415
AustinFash <asf128@scarletmail.rutgers.edu>
2025-06-04 00:07:57 -0400

    Used BoilerPlate and set up message board

--- app.js
+++ app.js
@@ -5,10 +5,35 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 app.get("/", (req, res) => {
-  res.render("index", { message: "EJS rocks!" });
+  res.render("index", { title: "Mini Messageboard", messages: messages })
 });
 
+app.get("/new", (req, res) => {
+  res.render("form", { messages });
+});
+
+app.post("/new", (req, res) => {
+  const { messageText, messageUser } = req.body;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/")
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
